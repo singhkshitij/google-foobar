925b791c104fd6f07df7f212d8cce93a3abb4d92
kshitij.singh <kshitij.singh@go-jek.com>
2020-02-29 22:44:59 +0530

    --ammend

--- bunnyPrisoner.java
+++ bunnyPrisoner.java
@@ -31,6 +31,38 @@ Solution.solution(5, 10)
 Output:
     96
 
+
+==== Solution =====
+
+
+So, what they have given here is a number sequence. So if you do some work on this
+| 7   12  18   25
+| 4    8   13   19
+| 2    5    9    14
+| 1    3    6    10
+
+You’d get a 2D array.
+
+Now if you take the first slice of it, I get 1,2,4,7 – This can be formulated for [n (n-1) / 2] + 1 where n is the place of the number starting with 1.
+
+Let’s take the second slice, 3,5,8,12 – This also can be formulated to [(n+1)(n) / 2] + 2 again where n is the place of the number starting with 1.
+
+If you see both of the above formulas, there is a pattern, where the counts related to “n” is getting increased by one with the last constant we add. To make our observation on this pattern is correct, let’s analyze the third slice, 6,9,13,18 – and that can be formulated for [(n+2)(n+1) / 2] + 3 where n starts with 1. So we can come to the conclusion that the observed pattern is the one we are looking for.
+
+With this we can get a general formula for the 2D array as below
+
+{[(x + [n – 1])] [([x – 1] + [n – 1])]} / 2 + x
+
+if we normalize this
+
+[(x + n – 1)(x + n – 2)] / 2 + x
+
+Here x is the horizontal position of an element and n is the vertical one, both start from 1. Also we can say n as y since they have mentioned it as y in the problem.
+
+Finally,
+
+[(x + y – 1)(x + y – 2)] / 2 + x
+
 */
 
 
